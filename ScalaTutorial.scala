a27ca78d9765a7b5c05e4ca92e1f84d2e467db87
jas75 <azoulayjason@gmail.com>
2017-09-24 16:16:21 +0200

    loops

--- ScalaTutorial.scala
+++ ScalaTutorial.scala
@@ -5,7 +5,13 @@ object ScalaTutorial {
 
 		val aList= List(1,2,3,4,5)
 
-		var eneList= for
+		// the value of i, after it cycles through this list,is goin to be store in the evenList
+		// var evenList= for{i <- 1 to 20 
+		//  if (i%2) == 0 } yield i
+
+
+		//  for(i<-evenList)
+		//  	println(i)
 
 		// while(i<=10){
 		// 	println(i)
@@ -31,6 +37,27 @@ object ScalaTutorial {
 		// for(i<-aList){
 		// 	println("List items " +i)
 		// }
+
+		//multiple variables increment
+		// for(i<- 1 to 5;j<-6 to 10){
+		// 	println("i : "+i)
+		// 	println("j :"+j)
+		// }
+
+		// function to stop a loop because there's no break in scala
+		// def printPrimes(){
+		// 	val primeList=List(1,2,3,4,7,11)
+		// 	for(i<- primeList){
+		// 		if(i==11){
+		// 			return // STOPs the program like a break
+		// 		}
+		// 		if(i!=1){
+		// 			println(i)
+		// 		}
+		// 	}
+		// }
+
+		// printPrimes
 
 
 	}
