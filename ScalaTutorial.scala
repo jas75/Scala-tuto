7e5f6db1ffb61893b5d72fbc93afddf57925dd56
jas75 <azoulayjason@gmail.com>
2017-09-24 15:59:53 +0200

    First commit

---
+++ ScalaTutorial.scala
@@ -0,0 +1,37 @@
+object ScalaTutorial {
+	def main(args:Array[String]){
+		var i= 0
+		val randLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+		val aList= List(1,2,3,4,5)
+
+		var eneList= for
+
+		// while(i<=10){
+		// 	println(i)
+		// 	i+=1
+		// }
+
+		// do{
+		// 	println(i)
+		// 	i+=1
+		// }
+		// while(i<=10)
+
+			// different type of loop
+		// for(i<- 1 to 10 ){
+		// 	println(i)
+		// }
+
+		// loops string or array
+		// for( i <- O until randLetters.length){
+		// 	println(randLetters(i))
+		// }
+
+		// for(i<-aList){
+		// 	println("List items " +i)
+		// }
+
+
+	}
+}
